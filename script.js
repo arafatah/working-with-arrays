3854c983f09c271023893d73f7ab90d990e67bf1
arafatah <arafathowlader890@gmail.com>
2025-01-12 11:30:43 +0600

    Add currency mapping and logging for supported currencies

--- script.js
+++ script.js
@@ -666,3 +666,13 @@ movements.forEach(function (movement, i, array) {
     console.log(`You withdraw ${i + 1}: ${Math.abs(movement)}`);
   }
 });
+ 
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
