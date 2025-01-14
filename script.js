110c473af1f835e31256e8b07aab241020a4cb21
arafatah <arafathowlader890@gmail.com>
2025-01-14 13:07:29 +0600

    Add account type categorization and group accounts by type

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  type: 'Premium',
 };
 
 const account2 = {
@@ -15,6 +16,7 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  type: 'basic',
 };
 
 const account3 = {
@@ -22,6 +24,7 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  type: 'standard',
 };
 
 const account4 = {
@@ -29,6 +32,7 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  type: 'Premium',
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -942,3 +946,6 @@ const groupedByActivity = Object.groupBy(accounts, account => {
 });
 
 console.log(groupedByActivity);
+
+const groupAccounts = Object.groupBy(accounts, account => account.type);
+console.log(groupAccounts);
